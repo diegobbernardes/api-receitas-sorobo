ba047de1d4ec0789002880deef62cbacdf8f73fa
Diego Bobsin Bernardes <diegob.bernardes@gmail.com>
2018-07-15 16:33:12 -0300

    API rest

---
+++ controllers/Receita.php
@@ -0,0 +1,64 @@
+<?php
+namespace controllers{
+	class Receita{
+		//Atributo para banco de dados
+		private $PDO;
+
+		/*
+		__construct
+		Conectando ao banco de dados
+		*/
+		function __construct(){
+			$this->PDO = new \PDO('mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_a240fb0bf187ac6; charset=UTF8', 'b6bd418f4f2cf7', 'f7c43c31'); //Conexão
+			$this->PDO->setAttribute( \PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION ); //habilitando erros do PDO
+		}
+		/*
+		lista
+		Listand imoveis
+		*/
+		public function lista(){
+			global $app;
+			$sth = $this->PDO->prepare("SELECT * FROM heroku_a240fb0bf187ac6.receitas;");
+			$sth->execute();
+			$result = $sth->fetchAll(\PDO::FETCH_ASSOC);
+			$app->render('default.php',["data"=>$result],200); 
+		}
+		/*
+		get
+		Pega receita pelo id
+		*/
+		public function get($id){
+			global $app;
+			$sth = $this->PDO->prepare("SELECT * FROM heroku_a240fb0bf187ac6.receitas WHERE idReceita = :id");
+			$sth ->bindValue(':id',$id);
+			$sth->execute();
+			$result = $sth->fetch(\PDO::FETCH_ASSOC);
+			$app->render('default.php',["data"=>$result],200); 
+		}
+        /*
+        get
+        Pega receita pelo ids dos ingredientes
+        */
+        public function getReceitaIngrediente($ids){
+            global $app;
+            $valida = true;
+            $ids_l = "" ;
+            foreach ($ids as $value) {
+                if($valida){
+                    $valida = false;
+                    $ids_l = $value;
+                }else{
+                    $ids_l = $ids_l . "," . $value;
+                }
+            }
+            $sth = $this->PDO->prepare("Select distinct  r.idReceita,r.NomeReceita,r.Imagem,r.Imagem from heroku_a240fb0bf187ac6.receitas r Inner Join heroku_a240fb0bf187ac6.receitaingrediente ri on r.idReceita = ri.idReceita where ri.idIngrediente IN ($ids_l)");
+
+
+            $sth->execute();
+            $result = $sth->fetchAll(\PDO::FETCH_ASSOC);
+            $app->render('default.php',["data"=>$result],200);
+        }
+
+
+	}
+}
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// Allow from any origin
+if(isset($_SERVER["HTTP_ORIGIN"]))
+{
+    // You can decide if the origin in $_SERVER['HTTP_ORIGIN'] is something you want to allow, or as we do here, just allow all
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+}
+else
+{
+    //No HTTP_ORIGIN set, so we allow any. You can disallow if needed here
+    header("Access-Control-Allow-Origin: *");
+}
+
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 600");    // cache for 10 minutes
+
+if($_SERVER["REQUEST_METHOD"] == "OPTIONS")
+{
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT"); //Make sure you remove those you do not want to support
+
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    //Just exit with 200 OK with the above headers for OPTIONS method
+    exit(0);
+}
+//Autoload
+$loader = require 'vendor/autoload.php';
+
+//Instanciando objeto
+$app = new \Slim\Slim(array(
+    'templates.path' => 'templates'
+));
+
+//Listando todas
+$app->get('/receitas/', function() use ($app){
+	(new \controllers\Receita($app))->lista();
+});
+
+//get receita
+$app->get('/receita/ingredientes/:id+', function($ids) use ($app){
+	(new \controllers\Receita($app))->getReceitaIngrediente($ids);
+});
+
+
+
+//Rodando aplicação
+$app->run();
